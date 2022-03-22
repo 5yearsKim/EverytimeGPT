83046bf284689f220f401b03e08d027bc92c75d3
hyunwoo <hypothesis22@gmail.com>
2022-03-22 06:55:22 +0000

    ..

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ MAX_SEQ_LEN = 256
 
 ''' train'''
 
-BS = 64 
+BS = 32
 EPOCHS = 5
 LR = 5e-5
--- trainer/criterion.py
+++ trainer/criterion.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 
+@tf.function
 def sparse_categorical_crossentropy(y_pred, y_true):
-    y_true_masked = tf.boolean_mask(y_true, tf.not_equal(y_true, 0))
-    y_pred_masked = tf.boolean_mask(y_pred, tf.not_equal(y_true, 0))
+    mask = tf.not_equal(y_true, 0)
+
+    y_true_masked = tf.boolean_mask(y_true, mask)
+    y_pred_masked = tf.boolean_mask(y_pred, mask)
 
     loss = tf.keras.losses.sparse_categorical_crossentropy(y_true_masked, y_pred_masked, from_logits=True)
+#    loss = tf.keras.losses.sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
     loss = tf.reduce_mean(loss)
     return loss
 
+@tf.function
 def masked_sparse_categorical_crossentropy(y_pred, y_true):
     y_true_masked = tf.boolean_mask(y_true, tf.not_equal(y_true, -1))
     y_pred_masked = tf.boolean_mask(y_pred, tf.not_equal(y_true, -1))
